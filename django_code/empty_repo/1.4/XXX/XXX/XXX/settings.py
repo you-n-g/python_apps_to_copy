b954aad650820301991213ea92a7c5fe374e4c10
Young Yang <afe.young@gmail.com>
2013-02-22 18:21:16 +0800

    micro modify

--- django_code/empty_repo/1.4/XXX/XXX/XXX/settings.py
+++ django_code/empty_repo/1.4/XXX/XXX/XXX/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-# STATIC_ROOT = ''
+# STATIC_ROOT = os.path.join(DIRNAME, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -75,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    # STATIC_ROOT,  # STATIC_ROOT 不能被包含在 STATICFILES_DIRS 中
     os.path.join(DIRNAME, 'static'),
 )
 
